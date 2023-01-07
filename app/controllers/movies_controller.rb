95d0ea7fb2c7b1ec33e9621bd73fbf17431ab65e
Lisa Grange <lisa.grange42@gmail.com>
2023-01-07 16:49:07 +0100

    Exercise done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,30 @@
 class MoviesController < ApplicationController
+
+  def show
+  end
+
+  def update
+    @movie.update(movie_params)
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path, status: :see_other
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movie_path(@movie)
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
 end
